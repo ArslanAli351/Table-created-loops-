e9833239c71cad26f15f975d7a91386e52ba7373
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-26 19:15:28 -0700

    table loops

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+
+let table=+prompt("Type your table name");
+for(let i = 1; i <=10; i++){
+    document.write("<span><br />"+table+" x "+i+" = "+table*i+"</span>")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let table=+prompt("Type your table name");
+// for(let i = 1; i <=20; i++){
+//     document.write("<br />"+table+" x "+i+" = "+table*i)}
+
+
+
+
+
+
+
+
+
+
+
